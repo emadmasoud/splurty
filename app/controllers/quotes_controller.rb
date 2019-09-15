7c2e5d8a2ccbdc1f6e988be1bffa4285bc29fa10
Emad Masoud <emad3000@gmail.com>
2019-09-15 16:14:37 +0000

    add profile link

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,8 +1,13 @@
 class QuotesController < ApplicationController
+
     def index
         @quote = Quote.order("RANDOM()").first
     end
-    
+
+    def new
+        @quote = Quote.new
+    end
+
     def create
         @quote = Quote.create(quote_params)
           if @quote.invalid?
@@ -10,12 +15,14 @@ class QuotesController < ApplicationController
           end
         redirect_to root_path
     end
-    def about
-    end
+
     private
 
         def quote_params
             params.require(:quote).permit(:saying, :author)
         end
-        
+    
+    def about
+    end
+
 end
